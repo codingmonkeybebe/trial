e757dd6bcac1a0b9aca11ec206c17f94517532bd
codingmonkeybebe <53258580+codingmonkeybebe@users.noreply.github.com>
2024-04-18 11:39:13 +0800

    Update BBCmain.py

--- pages/BBCmain.py
+++ pages/BBCmain.py
@@ -27,7 +27,7 @@ def findIRR():
 
 with col1:     
     capex = st.slider('Upgrade CapEx Cost',
-                        min_value=10.0, max_value=60.0,
+                        min_value=10.0, max_value=120.0,
                         value=20.0, step=0.5,format="$%fm",key='capex',on_change = findBBC)
 
 #     opex = st.slider('Operating Cost',
