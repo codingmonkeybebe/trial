2a3616974337474517d3d4f3aa9b4983aa7bdc36
codingmonkeybebe <53258580+codingmonkeybebe@users.noreply.github.com>
2024-08-15 16:17:49 +0800

    Update rateTry.py

--- pages/rateTry.py
+++ pages/rateTry.py
@@ -115,8 +115,8 @@ with st.container():
                 opexPV = -npf.pv(irrR/100/12,ecoLife*12,opex*dm,0)/mm
                 sbcR= sbcR0-deltaCpx
                 for j in range(1,deltaCpx*2+2,1):
-                    npvR=sbcR+opexPV
-                    bbc = npf.pmt(irrR/12,n*12, -npvR*mm, rv*mm)/dm
+                    #npvR=sbcR+opexPV
+                    bbc = npf.pmt(irrR/12,n*12, -(sbcR+opexPV)*mm, rv*mm)/dm
                     st.write(float("{:.1f}".format(float(bbc))),"")
                     sbcR= sbcR+1
                  
