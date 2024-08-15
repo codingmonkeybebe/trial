80157a5c1041274f7da926516947e106c302c7fb
codingmonkeybebe <53258580+codingmonkeybebe@users.noreply.github.com>
2024-08-15 15:54:09 +0800

    Update rateTry.py

--- pages/rateTry.py
+++ pages/rateTry.py
@@ -27,7 +27,7 @@ col3,col4 = st.columns([20,1])
 c3, c4 = st.columns([5,5])
 
 def findBBC():
-    st.session_state.irr=0.08
+    st.session_state.irr=8
     st.session_state.opexPV = -npf.pv(irr/100/12,ecoLife*12,opex*dm,0)/mm
     st.session_state.pv=(st.session_state.sbc+st.session_state.opexPV)*mm
     st.session_state.bbc=round(npf.pmt(st.session_state.irr/100/12,st.session_state.n*12,-st.session_state.pv,st.session_state.rv*mm)/dm,1)
