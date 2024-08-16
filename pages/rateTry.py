74711f9664ddeddef3071e57fb1710fdbb96673c
codingmonkeybebe <53258580+codingmonkeybebe@users.noreply.github.com>
2024-08-17 00:46:36 +0800

    Update rateTry.py

--- pages/rateTry.py
+++ pages/rateTry.py
@@ -42,7 +42,7 @@ def change_label_style(label, font_size, font_color, font_family):
     st.components.v1.html(html)
 
 label = "My text here"
-st.write(change_label_style(label, '20px','green','palatino linotype'))
+st.write(change_label_style(label, '20px','red','palatino linotype'))
 
 
 
