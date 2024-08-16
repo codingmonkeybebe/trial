77b74b89063bf896e25b05d5900323d2602c5470
codingmonkeybebe <53258580+codingmonkeybebe@users.noreply.github.com>
2024-08-16 10:03:25 +0800

    Update rateTry.py

--- pages/rateTry.py
+++ pages/rateTry.py
@@ -33,13 +33,12 @@ col3,col4 = st.columns([20,1])
 c3, c4 = st.columns([5,5])
 
 def finxXX():
-
-    for i in range(1, 50):
-        #st.session_state.irr=8
-        st.session_state.opexPV = -npf.pv(st.session_state.irr/100/12,ecoLife*12,opex*dm,0)/mm
-        findIRR() 
-        findBBC()
-        bbc=st.session_state.bbc
+    st.session_state.irr=defaultIRR
+    st.session_state.opexPV = -npf.pv(st.session_state.irr/100/12,ecoLife*12,opex*dm,0)/mm
+    findBBC()
+    #findIRR() 
+    #    
+    #    bbc=st.session_state.bbc
 
     
 
