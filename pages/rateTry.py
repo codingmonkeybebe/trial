a2249a7d7fe517776758ecd418978a54f0ec6bde
codingmonkeybebe <53258580+codingmonkeybebe@users.noreply.github.com>
2024-08-16 12:03:17 +0800

    Update rateTry.py

--- pages/rateTry.py
+++ pages/rateTry.py
@@ -40,7 +40,7 @@ def roundup(x):
 def finxXX():
 
     #for i in range(1,10):
-        #st.session_state.irr=irr
+   st.session_state.irr=irr
    st.session_state.opexPV = -npf.pv((st.session_state.irr-inflation)/100/12,ecoLife*12,opex*dm,0)/mm
    findBBC()
         #findIRR() 
@@ -51,10 +51,12 @@ def finxXX():
 
 def findBBC():
     #st.session_state.irr=8
+    st.session_state.irr=irr
     st.session_state.opexPV = -npf.pv((st.session_state.irr-inflation)/100/12,ecoLife*12,opex*dm,0)/mm
     st.session_state.pv=(st.session_state.sbc+st.session_state.opexPV+st.session_state.otherCapex)*mm
     st.session_state.bbc=round(npf.pmt(st.session_state.irr/100/12,st.session_state.n*12,-st.session_state.pv,st.session_state.rv*mm)/dm/utiizationFirm,1)
 def findIRR():
+    bbc=st.session_state.bbc
     st.session_state.opexPV = -npf.pv((st.session_state.irr-inflation)/100/12,ecoLife*12,opex*dm,0)/mm
     st.session_state.pv=(st.session_state.sbc+st.session_state.opexPV+st.session_state.otherCapex)*mm
     st.session_state.irr= round(100*npf.rate(st.session_state.n*12, st.session_state.bbc*dm, -st.session_state.pv, st.session_state.rv*mm)*12,5)
