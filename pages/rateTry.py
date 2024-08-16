0ab7705d5627942db1a8a1f53458ba3fb025c28f
codingmonkeybebe <53258580+codingmonkeybebe@users.noreply.github.com>
2024-08-16 17:30:31 +0800

    Update rateTry.py

--- pages/rateTry.py
+++ pages/rateTry.py
@@ -34,16 +34,29 @@ c3, c4 = st.columns([5,5])
 def roundup(x):
     return int(math.ceil(x / 100)) * 100#to the nearest 10th
 
-def finxXX():
-   st.session_state.opexPV = -npf.pv((st.session_state.irr-inflation)/100/12,ecoLife*12,opex*dm,0)/mm
-   st.session_state.pv=(st.session_state.sbc+st.session_state.opexPV+st.session_state.otherCapex)*mm
-   #findBBC()
+def finxXX():         
+    #find the End of economic life value of re-lease, opex, rv
+    i=st.session_state.irr/100/12 #interest rate in decimal and monthly basis
+    
+    RREndOfFirmFV=-npf.pv((i,(ecoLife-n)*12,releaseRate*dm,0)/mm #the fv of release rate at end of firm period
+    RVEndOfFirmFV=-npf.pv((i,(ecoLife-n)*12,0,rv)/mm #the fv of residual value at end of firm period
+    
+    i=(st.session_state.irr-inflation)/100/12 #interest rate in decimal and monthly basis 
+    escale=(1+inflation)**(n)
+    opexEndOfFirmFV=-npf.pv((i,(ecoLife-n)*12,opex*escale*dm,0)/mm #the fv of release rate at end of firm period
+    
+    st.session_state.fvEndOfFirm =RREndOfFirmFV + RVEndOfFirmFV + opexEndOfFirmFV
+
+    i=(st.session_state.irr-inflation)/100/12 #interest rate in decimal and monthly basis 
+    st.session_state.opexPV = -npf.pv((i,n*12,opex*dm,0)/mm  #PV of opex during the firm period
+    st.session_state.pv=(st.session_state.sbc+st.session_state.opexPV+st.session_state.otherCapex)
+    #findBBC()
 
 def findBBC():
     finxXX()
     i=st.session_state.irr/100/12 #interest rate in decimal and monthly basis
     term=st.session_state.n*12 #number of months
-    npv=st.session_state.pv #present value
+    npv=st.session_state.pv*mm #present value
     fv=st.session_state.rv*mm #future value
     adj=(dm*utiizationFirm)
     st.session_state.bbc=roundup(npf.pmt(i,term,-npv,fv)/adj)
@@ -51,7 +64,7 @@ def findIRR():
     finxXX()
     pmt=st.session_state.bbc*dm #monthly payment
     term=st.session_state.n*12 #number of months
-    npv=st.session_state.pv #present value
+    npv=st.session_state.pv*mm #present value
     fv=st.session_state.rv*mm #future value
     adj=12*100#from monthly to annual rate and in whole number
     st.session_state.irr= round(npf.rate(term, pmt, -npv, fv)*adj,1)
@@ -88,13 +101,21 @@ with st.container():
                             min_value=18, max_value=30,
                             value=25, step=1,format="%d yr",key='ecoLife',on_change = findBBC)
         
-        
         n = st.slider('Firm Period',
                             min_value=1, max_value=ecoLife,
                             value=10, step=1,format="%d yr",key='n',on_change = findBBC)
         irr = st.slider('Target IRR %',
                             min_value=4.0, max_value=15.0,
                             value=defaultIRR, step=0.1,format="%0.1f",key='irr',on_change = findBBC)
+        
+        
+
+        
+        
+        
+        
+        
+        
         defaultBBC=findBBC()
         bbc = st.slider('Daily Rate',
                             min_value=0, max_value=200000,
@@ -141,12 +162,12 @@ with st.container():
                 formatted_string = "{:.2f}".format(irrR*100)
                 st.write(formatted_string+"%")
                 
-                opexPV = -npf.pv((irrR-inflation/100)/12,ecoLife*12,opex*dm,0)/mm
+                opexPV = -npf.pv((irrR-inflation/100)/12,n*12,opex*dm,0)/mm
                 sbcR= sbcR0-deltaCpx
                 for j in range(1,deltaCpx*2+2,1):
                     npvR=sbcR+otherCapex+opexPV#sum all pv of capex and opex and dd and any other capex
-                    #st.write(irrR)
-                    bbc = roundup(npf.pmt(irrR/12,n*12, -(npvR)*mm, rv*mm)/dm/utiizationFirm)
+                    fv=st.session_state.fvEndOfFirm*mm
+                    bbc = roundup(npf.pmt(irrR/12,n*12, -(npvR)*mm, fv)/dm/utiizationFirm)
                     formatted_string = "${:.1f}".format(bbc/1000)
                     st.write(formatted_string,"k")
                     sbcR= sbcR+1
