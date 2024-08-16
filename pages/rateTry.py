f574a7d7f053736771e2335e7ce99fd276c27eaa
codingmonkeybebe <53258580+codingmonkeybebe@users.noreply.github.com>
2024-08-16 11:18:48 +0800

    Update rateTry.py

--- pages/rateTry.py
+++ pages/rateTry.py
@@ -111,7 +111,8 @@ with st.container():
         fsBBC = "{:,}".format(bbc)
         
         fsIRR = "{:.1f}".format(st.session_state.irr)
-        st.write("Recommendation: ",fsIRR,"%" " Daily Rate ",fsBBC," for ",n," yrs firm period")
+        fsN="{:.0d}".format(st.session_state.n)
+        st.write("Recommendation: ",fsIRR,"%" " Daily Rate ",fsBBC," for ",fsN," yrs firm period")
         sbcR0=round(sbc,1)
         deltaCpx=2
         bbcR=round(bbc,-1)
