16e58d8a688f87c9f4010a9425b785db447448db
codingmonkeybebe <53258580+codingmonkeybebe@users.noreply.github.com>
2024-08-16 18:26:43 +0800

    Update rateTry.py

--- pages/rateTry.py
+++ pages/rateTry.py
@@ -39,13 +39,14 @@ def findFV(int):
     i=int/100/12 #interest rate in decimal and monthly basis
     RREndOfFirmFV=-npf.pv(i,(ecoLife-n)*12,releaseRate*dm,0)/mm #the fv of release rate at end of firm period
     RVEndOfFirmFV=-npf.pv(i,(ecoLife-n)*12,0,rv)/mm #the fv of residual value at end of firm period
-
+    st.write(RREndOfFirmFV)
+    st.write(RVEndOfFirmFV)
 
     i=(int-inflation)/100/12 #interest rate in decimal and monthly basis 
     escale=(1+inflation/100)**(n)
 
     opexEndOfFirmFV=-npf.pv(i,(ecoLife-n)*12,opex*escale*dm,0)/mm #the fv of release rate at end of firm period
-    
+    st.write(opexEndOfFirmFV)
     i=(st.session_state.irr-inflation)/100/12 #interest rate in decimal and monthly basis 
     st.session_state.opexPV = -npf.pv(i,n*12,opex*dm,0)/mm  #PV of opex during the firm period
     st.session_state.pv=(st.session_state.sbc+st.session_state.opexPV+st.session_state.otherCapex)
