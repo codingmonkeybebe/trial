ebaa85fb3899e9947eb93b688bcd6249b047f871
codingmonkeybebe <53258580+codingmonkeybebe@users.noreply.github.com>
2024-08-16 12:05:00 +0800

    Update rateTry.py

--- pages/rateTry.py
+++ pages/rateTry.py
@@ -51,7 +51,7 @@ def finxXX():
 
 def findBBC():
     #st.session_state.irr=8
-    st.session_state.irr=irr
+    st.session_state.irr=findIRR()
     st.session_state.opexPV = -npf.pv((st.session_state.irr-inflation)/100/12,ecoLife*12,opex*dm,0)/mm
     st.session_state.pv=(st.session_state.sbc+st.session_state.opexPV+st.session_state.otherCapex)*mm
     st.session_state.bbc=round(npf.pmt(st.session_state.irr/100/12,st.session_state.n*12,-st.session_state.pv,st.session_state.rv*mm)/dm/utiizationFirm,1)
