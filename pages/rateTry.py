f2663bc97178f097a758691aaf1fe21f51058fea
codingmonkeybebe <53258580+codingmonkeybebe@users.noreply.github.com>
2024-08-17 00:40:48 +0800

    Update rateTry.py

--- pages/rateTry.py
+++ pages/rateTry.py
@@ -31,6 +31,29 @@ col3,col4 = st.columns([20,1])
 #col1, col2= st.columns([5,5])
 c3, c4 = st.columns([5,5])
 
+
+
+def change_label_style(label, font_size='12px', font_color='black', font_family='sans-serif'):
+    html = f"""
+    <script>
+        var elems = window.parent.document.querySelectorAll('p');
+        var elem = Array.from(elems).find(x => x.innerText == '{label}');
+        elem.style.fontSize = '{font_size}';
+        elem.style.color = '{font_color}';
+        elem.style.fontFamily = '{font_family}';
+    </script>
+    """
+    st.components.v1.html(html)
+
+label = "My text here"
+st.text_input(label)
+st.write(change_label_style(label, '20px'))
+
+
+
+
+
+
 def roundup(x):
     return int(math.ceil(x / 100)) * 100#to the nearest 10th
 
