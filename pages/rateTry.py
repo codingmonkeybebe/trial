a45b0ad29b89fba552fc60cdafa21b452d865ded
codingmonkeybebe <53258580+codingmonkeybebe@users.noreply.github.com>
2024-08-16 13:04:23 +0800

    Update rateTry.py

--- pages/rateTry.py
+++ pages/rateTry.py
@@ -97,17 +97,17 @@ with st.container():
         #formatted_string = "{:,}".format(bbc)
         st.write("Total Cost: ")#+formatted_string+"mn")
 
-        fsBBC= "{:,}".format(bbc)
+        fsBBC= "${:,}".format(bbc)
         fsIRR = "{:.1f}".format(st.session_state.irr)
         fsN="{:d}".format(st.session_state.n)
-        fsTOTALCAPEX="{:0.1f}".format(st.session_state.sbc+st.session_state.otherCapex)
-        fsOPEX= "{:,}".format(opex)
-        fsRELEASERATE= "{:,}".format(releaseRate)
+        fsTOTALCAPEX="${:0.1f}".format(st.session_state.sbc+st.session_state.otherCapex)
+        fsOPEX= "${:,}".format(opex)
+        fsRELEASERATE= "${:,}".format(releaseRate)
         st.write("Recommendation: ",fsIRR,"%" " Daily Rate ",fsBBC,"pd,",
                 " for firm period ",fsN," yrs,",
-                " with Capex$ "+fsTOTALCAPEX,"m,",
-                " with Opex$ "+fsOPEX,"pd,",
-                " with release rate$ "+fsRELEASERATE,"pd")
+                " with Capex "+fsTOTALCAPEX,"m,",
+                " with Opex "+fsOPEX,"pd,",
+                " with release rate "+fsRELEASERATE,"pd")
         
         
         sbcR0=round(sbc,1)
